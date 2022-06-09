d3723c5f1501c90e59991ded19c10b54068abab6
Leroy Rosales <leroyrosales@gmail.com>
2022-06-09 10:38:40 -0500

    Adds social icons

--- src/components/NavBar.component.jsx
+++ src/components/NavBar.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
   return (
@@ -7,42 +8,84 @@ export default function NavBar() {
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
           <div className="flex items-center">
-            <a href="#">
-              <span className="sr-only">Workflow</span>
+            <NavLink to="/" exact>
+              <span className="sr-only">Vite &amp; Sanity</span>
               <img
                 className="h-10 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
                 alt=""
               />
-            </a>
+            </NavLink>
             <div className="hidden ml-10 space-x-8 lg:block">
-                <NavLink to="/" className="text-base font-medium text-white hover:text-indigo-400" exact>
-                    Home
-                </NavLink>
-                <NavLink to="/about" className="text-base font-medium text-white hover:text-indigo-400" exact>
-                    About
-                </NavLink>
-                <NavLink to="/projects" className="text-base font-medium text-white hover:text-indigo-400" exact>
-                    Projects
-                </NavLink>
-                <NavLink to="/posts" className="text-base font-medium text-white hover:text-indigo-400" exact>
-                    Blog
-                </NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-white font-bold"
+                    : "text-base font-medium text-white hover:text-indigo-400"
+                }
+                exact
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-white font-bold"
+                    : "text-base font-medium text-white hover:text-indigo-400"
+                }
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="/projects"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-white font-bold"
+                    : "text-base font-medium text-white hover:text-indigo-400"
+                }
+              >
+                Projects
+              </NavLink>
+              <NavLink
+                to="/posts"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-white font-bold"
+                    : "text-base font-medium text-white hover:text-indigo-400"
+                }
+              >
+                Blog
+              </NavLink>
             </div>
           </div>
           <div className="ml-10 space-x-4">
-            <a
-              href="#"
-              className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
-            >
-              Sign in
-            </a>
-            <a
-              href="#"
-              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
-            >
-              Sign up
-            </a>
+          <SocialIcon
+              url="#"
+              network="facebook"
+              className="hover:opacity-75"
+              target="_blank"
+              fgColor="#fff"
+              style={{ height: 35, width: 35 }}
+            />
+            <SocialIcon
+              url="#"
+              network="linkedin"
+              className="hover:opacity-75"
+              target="_blank"
+              fgColor="#fff"
+              style={{ height: 35, width: 35 }}
+            />
+
+<SocialIcon
+              url="#"
+              network="twitter"
+              className="hover:opacity-75"
+              target="_blank"
+              fgColor="#fff"
+              style={{ height: 35, width: 35 }}
+            />
           </div>
         </div>
         <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
