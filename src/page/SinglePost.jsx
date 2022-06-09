b4cbfb266dc25f557e438ff89d0de65b5d88eb6b
Leroy Rosales <leroyrosales@gmail.com>
2022-06-09 12:05:27 -0500

    Adds single post view

--- src/page/SinglePost.jsx
+++ src/page/SinglePost.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client";
+import { Link } from "react-router-dom";
+import ImageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = ImageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function SinglePost() {
+  const [singlePost, setSinglePost] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == "${slug}"]{
+      title,
+      _id,
+      slug,
+      mainImage{
+        asset->{
+          _id,
+          url
+        },
+        alt
+      },
+      body,
+    }`
+      )
+      .then((data) => setSinglePost(data[0]))
+      .catch(console.error);
+  }, [slug]);
+
+  if (!singlePost) return <div>Loading...</div>;
   return (
-    <div>SinglePost</div>
-  )
+    <main className="bg-green-100 min-h-screen p-12">
+      <article>
+        <header>
+          <h1>{singlePost.title}</h1>
+          <img
+            src={urlFor(singlePost.mainImage.asset.url)}
+            alt={singlePost.title}
+          />
+        </header>
+        <section className="mx-auto prose lg:prose-xl">
+<BlockContent blocks={singlePost.body} projectId="3tx74omu" dataset="production"/>
+        </section>
+      </article>
+    </main>
+  );
 }
